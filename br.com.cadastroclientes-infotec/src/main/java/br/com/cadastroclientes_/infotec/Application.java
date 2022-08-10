71e8c40b48697b17b356a82520c3d688515c5812
Denilson Ribeiro <Denilson Ribeiro@DESKTOP-QGSK3C4>
2022-08-10 03:25:57 -0300

    GitHub está pronto? Se´r?

--- br.com.cadastroclientes-infotec/src/main/java/br/com/cadastroclientes_/infotec/Application.java
+++ br.com.cadastroclientes-infotec/src/main/java/br/com/cadastroclientes_/infotec/Application.java
@@ -11,5 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+        
+        //TESTANDO GITHUB
     }
 }
